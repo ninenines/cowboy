20204fd33ef5040428f6d9e3c8e640a7b16abc9d
Loïc Hoguin <essen@dev-extend.eu>
2011-10-26 19:23:06 +0200

    Add 'Accept-Encoding' to cowboy_http_req:parse_header/2

--- src/cowboy_http_req.erl
+++ src/cowboy_http_req.erl
@@ -201,6 +201,7 @@ parse_header(Name, Req=#http_req{p_headers=PHeaders}) ->
 %% @doc Default values for semantic header parsing.
 -spec parse_header_default(http_header()) -> any().
 parse_header_default('Accept') -> [];
+parse_header_default('Accept-Encoding') -> [];
 parse_header_default('Connection') -> [];
 parse_header_default(_Name) -> undefined.
 
@@ -213,6 +214,11 @@ parse_header(Name, Req, Default) when Name =:= 'Accept' ->
 	parse_header(Name, Req, Default,
 		fun (Value) ->
 			cowboy_http:list(Value, fun cowboy_http:media_range/2)
+		end);
+parse_header(Name, Req, Default) when Name =:= 'Accept-Encoding' ->
+	parse_header(Name, Req, Default,
+		fun (Value) ->
+			cowboy_http:list(Value, fun cowboy_http:token_ci/2)
 		end);
 parse_header(Name, Req, Default) when Name =:= 'Connection' ->
 	parse_header(Name, Req, Default,
