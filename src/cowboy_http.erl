46c1a79fa510b433018bf0d742aca2b661606d0a
Ali Sabil <ali.sabil@tillitech.com>
2012-03-02 05:26:44 +0100

    Make media_type/2 more forgiving
    Certain user agents send slightly invalid media types, like the
    following: "text/html, image/gif, image/jpeg, *; q=.2, */*; q=.2"
    
    The user agent with which this behavior was observed presented itself
    with the User-Agent string: "Mozilla/5.0 (Macintosh; U; Intel Mac OS X
    10.6; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6 (FlipboardProxy/0.0.5;
    +http://flipboard.com/browserproxy)"

--- src/cowboy_http.erl
+++ src/cowboy_http.erl
@@ -156,6 +156,11 @@ media_type(Data, Fun) ->
 					fun (_Rest2, <<>>) -> {error, badarg};
 						(Rest2, SubType) -> Fun(Rest2, Type, SubType)
 					end);
+			(Rest, <<"*">> = Type) ->
+				token_ci(<<"*", Rest/binary>>,
+					fun (_Rest2, <<>>) -> {error, badarg};
+						(Rest2, SubType) -> Fun(Rest2, Type, SubType)
+					end);
 			(_Rest, _Type) -> {error, badarg}
 		end).
 
@@ -676,6 +681,8 @@ quoted_string(<< C, Rest/binary >>, Fun, Acc) ->
 -spec qvalue(binary(), fun()) -> any().
 qvalue(<< $0, $., Rest/binary >>, Fun) ->
 	qvalue(Rest, Fun, 0, 100);
+qvalue(<< $., Rest/binary >>, Fun) ->
+	qvalue(Rest, Fun, 0, 100);
 qvalue(<< $0, Rest/binary >>, Fun) ->
 	Fun(Rest, 0);
 qvalue(<< $1, $., $0, $0, $0, Rest/binary >>, Fun) ->
@@ -894,6 +901,13 @@ media_range_list_test_() ->
 				[{<<"level">>, <<"1">>}, {<<"quoted">>, <<"hi hi hi">>}]}, 123,
 				[<<"standalone">>, {<<"complex">>, <<"gits">>}]},
 			{{<<"text">>, <<"plain">>, []}, 1000, []}
+		]},
+		{<<"text/html, image/gif, image/jpeg, *; q=.2, */*; q=.2">>, [
+			{{<<"text">>, <<"html">>, []}, 1000, []},
+			{{<<"image">>, <<"gif">>, []}, 1000, []},
+			{{<<"image">>, <<"jpeg">>, []}, 1000, []},
+			{{<<"*">>, <<"*">>, []}, 200, []},
+			{{<<"*">>, <<"*">>, []}, 200, []}
 		]}
 	],
 	[{V, fun() -> R = list(V, fun media_range/2) end} || {V, R} <- Tests].
