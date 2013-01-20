e2c77e1b64a7dd7eb2a258ba9399fa430f9d1eb4
Ivan Lisenkov <ivan@ivlis.com>
2013-01-20 18:23:39 +0400

    Implement authorization header parsing
    Basic HTTP authorization according to RFC 2617 is implemented.
    Added an example of its usage with REST handler.

---
+++ examples/basic_auth/src/rest_hello_world_app.erl
@@ -0,0 +1,25 @@
+%% Feel free to use, reuse and abuse the code in this file.
+
+%% @private
+-module(rest_hello_world_app).
+-behaviour(application).
+
+%% API.
+-export([start/2]).
+-export([stop/1]).
+
+%% API.
+
+start(_Type, _Args) ->
+	Dispatch = [
+		{'_', [
+			{[], toppage_handler, []}
+		]}
+	],
+	{ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
+		{env, [{dispatch, Dispatch}]}
+	]),
+	rest_hello_world_sup:start_link().
+
+stop(_State) ->
+	ok.
