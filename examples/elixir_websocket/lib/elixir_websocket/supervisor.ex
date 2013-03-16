2aea615020bc3d446b15cadf8860f8a0fd9da100
Michaël Heyvaert <micheyvaert@hotmail.com>
2013-03-16 13:40:56 +0100

    add elixir websocket example, doesn't work yet

---
+++ examples/elixir_websocket/lib/elixir_websocket/supervisor.ex
@@ -0,0 +1,12 @@
+defmodule ElixirWebsocket.Supervisor do
+  use Supervisor.Behaviour
+
+  def start_link do
+    :supervisor.start_link(__MODULE__, [])
+  end
+
+  def init([]) do
+    children = []
+    supervise children, strategy: :one_for_one
+  end
+end
