7ab12d1b66910d3651f7b2311a2fb6d17b9e4f31
Loïc Hoguin <essen@ninenines.eu>
2013-09-07 14:13:51 +0200

    Fix wrong header sent in echo_post example

--- examples/echo_post/src/toppage_handler.erl
+++ examples/echo_post/src/toppage_handler.erl
@@ -29,8 +29,9 @@ maybe_echo(_, _, Req) ->
 echo(undefined, Req) ->
 	cowboy_req:reply(400, [], <<"Missing echo parameter.">>, Req);
 echo(Echo, Req) ->
-	cowboy_req:reply(200,
-		[{<<"content-encoding">>, <<"utf-8">>}], Echo, Req).
+	cowboy_req:reply(200, [
+		{<<"content-type">>, <<"text/plain; charset=utf-8">>}
+	], Echo, Req).
 
 terminate(_Reason, _Req, _State) ->
 	ok.
