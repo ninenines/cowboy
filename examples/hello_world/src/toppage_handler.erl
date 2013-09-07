335676876b3f23cdc19450bd5eea68105e8fe142
Loïc Hoguin <essen@ninenines.eu>
2013-09-07 15:33:40 +0200

    Convert the hello world example to a release

--- examples/hello_world/src/toppage_handler.erl
+++ examples/hello_world/src/toppage_handler.erl
@@ -7,11 +7,13 @@
 -export([handle/2]).
 -export([terminate/3]).
 
-init(_Transport, Req, []) ->
+init(_Type, Req, []) ->
 	{ok, Req, undefined}.
 
 handle(Req, State) ->
-	{ok, Req2} = cowboy_req:reply(200, [], <<"Hello world!">>, Req),
+	{ok, Req2} = cowboy_req:reply(200, [
+		{<<"content-type">>, <<"text/plain">>}
+	], <<"Hello world!">>, Req),
 	{ok, Req2, State}.
 
 terminate(_Reason, _Req, _State) ->
