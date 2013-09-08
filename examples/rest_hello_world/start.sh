81843414b00f3d1829db5f105206396985f1196c
Loïc Hoguin <essen@ninenines.eu>
2013-09-08 12:22:30 +0200

    Convert the REST hello world example to a release

--- examples/rest_hello_world/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-erl -pa ebin deps/*/ebin -s rest_hello_world \
-	-eval "io:format(\"Get HTML: curl -i http://localhost:8080~n\")." \
-	-eval "io:format(\"Get JSON: curl -i -H \\\"Accept: application/json\\\" http://localhost:8080~n\")." \
-	-eval "io:format(\"Get text: curl -i -H \\\"Accept: text/plain\\\" http://localhost:8080~n\")." \
-	-eval "io:format(\"Get a 406: curl -i -H \\\"Accept: text/css\\\" http://localhost:8080~n\")."
