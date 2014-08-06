260fc1e60b6cc231e026d0aa28a24bf7dec7539a
Loïc Hoguin <essen@ninenines.eu>
2014-08-06 11:32:58 +0200

    Handle absolute URIs that lack a path entirely

--- src/cowboy_protocol.erl
+++ src/cowboy_protocol.erl
@@ -191,6 +191,9 @@ parse_uri_skip_host(<< C, Rest/bits >>, State, Method) ->
 	case C of
 		$\r -> error_terminate(400, State);
 		$/ -> parse_uri_path(Rest, State, Method, <<"/">>);
+		$\s -> parse_version(Rest, State, Method, <<"/">>, <<>>);
+		$? -> parse_uri_query(Rest, State, Method, <<"/">>, <<>>);
+		$# -> skip_uri_fragment(Rest, State, Method, <<"/">>, <<>>);
 		_ -> parse_uri_skip_host(Rest, State, Method)
 	end.
 
