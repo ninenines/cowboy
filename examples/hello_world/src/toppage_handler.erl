3cf768b4cf82a0de4209fdabbb018802b512bf34
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-11-06 00:31:27 -0300

    Use the correct callbacks for an http handler.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- examples/hello_world/src/toppage_handler.erl
+++ examples/hello_world/src/toppage_handler.erl
@@ -3,10 +3,16 @@
 %% @doc Hello world handler.
 -module(toppage_handler).
 
--export([init/2]).
+-export([init/3, handle/2, terminate/3]).
 
-init(Req, Opts) ->
-	Req2 = cowboy_req:reply(200, [
-		{<<"content-type">>, <<"text/plain">>}
-	], <<"Hello world!">>, Req),
-	{ok, Req2, Opts}.
+init(_Type, Req, _Opts) ->
+    {ok, Req, no_state}.
+
+handle(Req, State) ->
+    {ok, Req2} = cowboy_req:reply(200, [
+        {<<"content-type">>, <<"text/plain">>}
+    ], <<"Hello World!">>, Req),
+    {ok, Req2, State}.
+
+terminate(_Reason, Req, State) ->
+    ok.
