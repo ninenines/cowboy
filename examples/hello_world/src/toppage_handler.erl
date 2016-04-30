ff936ff0ee62731d54ec35335355f76577044443
Loïc Hoguin <essen@ninenines.eu>
2016-04-30 14:41:10 +0200

    Fix hello_world example

--- examples/hello_world/src/toppage_handler.erl
+++ examples/hello_world/src/toppage_handler.erl
@@ -6,7 +6,7 @@
 -export([init/2]).
 
 init(Req, Opts) ->
-	Req2 = cowboy_req:reply(200, [
-		{<<"content-type">>, <<"text/plain">>}
-	], <<"Hello world!">>, Req),
-	{ok, Req2, Opts}.
+	cowboy_req:reply(200, #{
+		<<"content-type">> => <<"text/plain">>
+	}, <<"Hello world!">>, Req),
+	{ok, Req, Opts}.
