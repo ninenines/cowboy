35934c6efd9d3000e164740d73015460e5528d5f
sergey-ivlev <isergey.git@gmail.com>
2016-07-26 12:26:58 +0300

    fix echo_get example

--- examples/echo_get/src/toppage_handler.erl
+++ examples/echo_get/src/toppage_handler.erl
@@ -7,12 +7,12 @@
 
 init(Req, Opts) ->
 	Method = cowboy_req:method(Req),
-	#{echo := Echo} = cowboy_req:match_qs([echo], Req),
+	#{echo := Echo} = cowboy_req:match_qs([{echo, [], undefined}], Req),
 	echo(Method, Echo, Req),
 	{ok, Req, Opts}.
 
 echo(<<"GET">>, undefined, Req) ->
-	cowboy_req:reply(400, [], <<"Missing echo parameter.">>, Req);
+	cowboy_req:reply(400, #{}, <<"Missing echo parameter.">>, Req);
 echo(<<"GET">>, Echo, Req) ->
 	cowboy_req:reply(200, #{
 		<<"content-type">> => <<"text/plain; charset=utf-8">>
