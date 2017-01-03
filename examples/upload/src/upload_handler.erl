8b68dddba14280c9cce4fc8d29b57b984b1a5e58
Robert Balogh <ethrbh@gmail.com>
2017-01-03 20:27:05 +0100

    Allow to upload file with any size.

--- examples/upload/src/upload_handler.erl
+++ examples/upload/src/upload_handler.erl
@@ -6,10 +6,44 @@
 -export([init/2]).
 
 init(Req, Opts) ->
-	{ok, Headers, Req2} = cowboy_req:read_part(Req),
-	{ok, Data, Req3} = cowboy_req:read_part_body(Req2),
-	{file, <<"inputfile">>, Filename, ContentType, _TE}
-		= cow_multipart:form_data(Headers),
-	io:format("Received file ~p of content-type ~p as follow:~n~p~n~n",
-		[Filename, ContentType, Data]),
-	{ok, Req3, Opts}.
+	{Data, Req2} = do_init(Req),
+	{ok, cowboy_req:reply(200, #{}, term_to_binary(Data), Req2), Opts}.
+
+do_init(Req) ->
+	{Data, Req2} = acc_multipart(Req, []),
+	
+	[{Headers, Body}] = Data,
+	
+	{_HeaderPart_1, HeaderPart_2 } = cow_multipart:parse_content_disposition(erlang:term_to_binary(Headers)),
+	{<<"filename">>, FilenameBin} = lists:keyfind(<<"filename">>, 1, HeaderPart_2),
+	FileName = erlang:binary_to_list(FilenameBin),
+	
+	%% Put the file into the current directory
+	FileWriteRes = file:write_file(FileName, Body),
+	
+	error_logger:info_report(["Recived file has been saved", 
+							  {headers, Headers},
+							  {result, FileWriteRes},
+							  {fileName, FileName},
+							  {path, os:cmd("pwd")}
+							 ]),
+	
+	{Data, Req2}.
+
+acc_multipart(Req0, Acc) ->
+	case cowboy_req:read_part(Req0) of
+		{ok, Headers, Req1} ->
+			{ok, Body, Req} = stream_body(Req1, <<>>),
+			acc_multipart(Req, [{Headers, Body}|Acc]);
+		{done, Req} ->
+			{lists:reverse(Acc), Req}
+	end.
+
+stream_body(Req0, Acc) ->
+	case cowboy_req:read_part_body(Req0) of
+		{more, Data, Req} ->
+			stream_body(Req, << Acc/binary, Data/binary >>);
+		{ok, Data, Req} ->
+			{ok, << Acc/binary, Data/binary >>, Req}
+	end.
+
