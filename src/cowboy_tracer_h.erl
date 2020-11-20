500907066d84185ff967720a62d9490536b90a64
Sebastian Strollo <seb@strollo.org>
2020-11-21 00:28:24 +0100

    Also include trace messages when timestamp flag isn't used

--- src/cowboy_tracer_h.erl
+++ src/cowboy_tracer_h.erl
@@ -159,7 +159,8 @@ tracer_process(StreamID, Req=#{pid := Parent}, Opts=#{tracer_callback := Fun}) -
 
 tracer_loop(Parent, Opts=#{tracer_callback := Fun}, State0) ->
 	receive
-		Msg when element(1, Msg) =:= trace_ts ->
+		Msg when (element(1, Msg) =:= trace_ts) orelse
+			 (element(1, Msg) =:= trace) ->
 			State = Fun(Msg, State0),
 			tracer_loop(Parent, Opts, State);
 		{'EXIT', Parent, Reason} ->
