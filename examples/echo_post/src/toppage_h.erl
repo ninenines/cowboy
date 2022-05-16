4907734a83b1081e170bf48dbf22f27e800823d0
Anatolii Kosorukov <java1cprog@yandex.ru>
2022-05-16 14:34:43 +0300

    Fix echo_post example
    Fix input parameter of `cowboy_req:reply` function.

--- examples/echo_post/src/toppage_h.erl
+++ examples/echo_post/src/toppage_h.erl
@@ -16,13 +16,13 @@ maybe_echo(<<"POST">>, true, Req0) ->
 	Echo = proplists:get_value(<<"echo">>, PostVals),
 	echo(Echo, Req);
 maybe_echo(<<"POST">>, false, Req) ->
-	cowboy_req:reply(400, [], <<"Missing body.">>, Req);
+	cowboy_req:reply(400, #{}, <<"Missing body.">>, Req);
 maybe_echo(_, _, Req) ->
 	%% Method not allowed.
 	cowboy_req:reply(405, Req).
 
 echo(undefined, Req) ->
-	cowboy_req:reply(400, [], <<"Missing echo parameter.">>, Req);
+	cowboy_req:reply(400, #{}, <<"Missing echo parameter.">>, Req);
 echo(Echo, Req) ->
 	cowboy_req:reply(200, #{
 		<<"content-type">> => <<"text/plain; charset=utf-8">>
