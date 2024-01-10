9bfc95d17f7faa25bcc33d41fcd9b0b92756f2fb
geeksilva97 <edigleyssonsilva@gmail.com>
2024-01-10 15:46:18 -0300

    remove macros

--- src/cowboy_req.erl
+++ src/cowboy_req.erl
@@ -15,9 +15,6 @@
 
 -module(cowboy_req).
 
--define(INVALID_COOKIE_HEADER_ERROR, exit({response_error, invalid_header,
-    'The set-cookie header is special and must be set using cowboy_req:set_resp_cookie/3,4.'})).
-
 %% Request.
 -export([method/1]).
 -export([version/1]).
@@ -722,7 +719,8 @@ set_resp_cookie(Name, Value, Req, Opts) ->
 -spec set_resp_header(binary(), iodata(), Req)
 	-> Req when Req::req().
 set_resp_header(<<"set-cookie">>, _, _) ->
-	?INVALID_COOKIE_HEADER_ERROR;
+	exit({response_error, invalid_header,
+		'The set-cookie header is special and must be set using cowboy_req:set_resp_cookie/3,4.'});
 set_resp_header(Name, Value, Req=#{resp_headers := RespHeaders}) ->
 	Req#{resp_headers => RespHeaders#{Name => Value}};
 set_resp_header(Name,Value, Req) ->
@@ -731,7 +729,8 @@ set_resp_header(Name,Value, Req) ->
 -spec set_resp_headers(cowboy:http_headers(), Req)
 	-> Req when Req::req().
 set_resp_headers(#{<<"set-cookie">> := _}, _) ->
-	?INVALID_COOKIE_HEADER_ERROR;
+	exit({response_error, invalid_header,
+		'The set-cookie header is special and must be set using cowboy_req:set_resp_cookie/3,4.'});
 set_resp_headers(Headers, Req=#{resp_headers := RespHeaders}) ->
 	Req#{resp_headers => maps:merge(RespHeaders, Headers)};
 set_resp_headers(Headers, Req) ->
@@ -789,7 +788,8 @@ inform(_, _, #{has_sent_resp := _}) ->
 	exit({response_error, response_already_sent,
 		'The final response has already been sent.'});
 inform(_, #{<<"set-cookie">> := _}, _) ->
-	?INVALID_COOKIE_HEADER_ERROR;
+	exit({response_error, invalid_header,
+		'The set-cookie header is special and must be set using cowboy_req:set_resp_cookie/3,4.'});
 inform(Status, Headers, Req) when is_integer(Status); is_binary(Status) ->
 	cast({inform, Status, Headers}, Req).
 
@@ -810,7 +810,8 @@ reply(_, _, _, #{has_sent_resp := _}) ->
 	exit({response_error, response_already_sent,
 		'The final response has already been sent.'});
 reply(_, #{<<"set-cookie">> := _}, _, _) ->
-	?INVALID_COOKIE_HEADER_ERROR;
+	exit({response_error, invalid_header,
+				'The set-cookie header is special and must be set using cowboy_req:set_resp_cookie/3,4.'});
 reply(Status, Headers, {sendfile, _, 0, _}, Req)
 		when is_integer(Status); is_binary(Status) ->
 	do_reply(Status, Headers#{
@@ -869,7 +870,8 @@ stream_reply(_, _, #{has_sent_resp := _}) ->
 	exit({response_error, response_already_sent,
 		'The final response has already been sent.'});
 stream_reply(_, #{<<"set-cookie">> := _}, _) ->
-	?INVALID_COOKIE_HEADER_ERROR;
+	exit({response_error, invalid_header,
+		'The set-cookie header is special and must be set using cowboy_req:set_resp_cookie/3,4.'});
 %% 204 and 304 responses must NOT send a body. We therefore
 %% transform the call to a full response and expect the user
 %% to NOT call stream_body/3 afterwards. (RFC7230 3.3)
@@ -922,7 +924,8 @@ stream_events(Events, IsFin, Req=#{has_sent_resp := headers}) ->
 
 -spec stream_trailers(cowboy:http_headers(), req()) -> ok.
 stream_trailers(#{<<"set-cookie">> := _}, _) ->
-	?INVALID_COOKIE_HEADER_ERROR;
+	exit({response_error, invalid_header,
+		'The set-cookie header is special and must be set using cowboy_req:set_resp_cookie/3,4.'});
 stream_trailers(Trailers, Req=#{has_sent_resp := headers}) ->
 	cast({trailers, Trailers}, Req).
 
