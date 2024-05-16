662f6af98269f78460cf6749f49f241156520e13
Björn Svensson <bjorn.a.svensson@est.tech>
2024-05-16 12:23:15 +0200

    Correcting TransOpts in send_timeout_close tests

--- test/http2_SUITE.erl
+++ test/http2_SUITE.erl
@@ -449,8 +449,8 @@ graceful_shutdown_listener_timeout(Config) ->
 send_timeout_close(Config) ->
 	doc("Check that connections are closed on send timeout."),
 	TransOpts = #{
-		port => 0,
 		socket_opts => [
+			{port, 0},
 			{send_timeout, 100},
 			{send_timeout_close, true},
 			{sndbuf, 10}
